89e4c31442d412ea6f2c07679de7d74f524081e3
Deerop <xchenbox@gmail.com>
2020-08-03 23:27:15 -0700

    Add gitignore

---
+++ motorControl.java
@@ -0,0 +1,11 @@
+public class motorControl {
+
+	public static int leftMotorDegrees() {
+		return 0;
+	}
+
+	public static int rightMotorDegrees() {
+		return 0;
+	}
+    
+}
---
+++ simulator.java
@@ -0,0 +1,39 @@
+import java.lang.Math;
+
+public class simulator{
+    private static double x;
+    private static double y;
+    private static double rot;
+
+    public static int getX(){
+        return (int) x;
+    }
+
+    public static int  getRotation(){
+        return (int) rot;
+    }
+
+    public static int getY(){
+        return (int) y;
+    }
+
+    //take info from motor control
+    public static void update(){
+        int left = motorControl.leftMotorDegrees();
+        int right = motorControl.rightMotorDegrees();
+
+        double forwards = (left + right)/2;
+        double rotation = (right - left)/20;
+
+        double yChange = forwards * Math.sin(rot);
+        double xChange = forwards * Math.cos(rot);
+
+        x += xChange;
+        y += yChange;
+        rot += rotation;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from VsCode");
+    }
+}
