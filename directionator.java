548198dfcfe0bf40d206a2cf12b0b33eb402ea24
Aung Paing Soe <aungpaingsoe@Aung-Paing-Soe-MacBook-Air.local>
2020-08-06 15:52:52 -0700

    Added 'int's

--- directionator.java
+++ directionator.java
@@ -1,3 +1,11 @@
 public void setDestination(x,y){
-    
+    x = 
+}
+int goForwardsOrBackwards(){
+    int goForward = 1;
+    int goBackwards = -1;
+    int noMovement = 0;
+}
+int getAngleRotation(){
+    return 0;
 }
