4da8b70e46a28f2067e4870ec8670af4db62daca
Austin Shih <austin2shih@yahoo.com>
2020-08-07 21:04:11 -0700

    Fixed Syntax stuff

--- src/motorControl.java
+++ src/motorControl.java
@@ -5,47 +5,45 @@ public class motorControl{
 	int bot_diameter = 20;
 	int rotated = 0;
 
-	public static int leftMotorDegrees(int desired_rot, int direction){
-		int direction = directionator.goForwardsOrBackwards()
-		int desired_rot = directionator.getAngleRotation()
-		if(Math.abs(rotated) < Math.abs(desired_rot)){
-			int rot_direction = -desired_rot/Math.abs(desired_rot)
+	public static int leftMotorDegrees(){
+		int direction = directionator.goForwardsOrBackwards();
+		int desired_rot = directionator.getAngleRotation();
+		int x_dist = abs(simulator.getX() - newX);
+		int y_dist = abs(simulator.getY() - newY);
+
+		if(Math.abs(rotated) < Math.abs(desired_rot)) {
+			int rot_direction = -desired_rot / Math.abs(desired_rot);
 			rotated += 10;
-			int distance_to_move = Math.PI*bot_diameter*(10/360);
-			return rot_direction * distance_to_move*360 / wheel_circum;
+			int distance_to_move = Math.PI * bot_diameter * (10 / 360);
+			return rot_direction * distance_to_move * 360 / wheel_circum;
 		}
-
-		int x_dist = abs(simulator.getX() - newX)
-		int y_dist = abs(simulator.getY() - newY)
-
-		else if(x_dist < 3 and y_dist < 3){
-			return 36*direction
+		else if(x_dist < 3 && y_dist < 3){
+			return 36*direction;
 		}
 		else{
-			rotated = 0
-			return 0
+			rotated = 0;
+			return 0;
 		}
 	}
 
 	public static int rightMotorDegrees(){
-		int direction = directionator.goForwardsOrBackwards()
-		int desired_rot = directionator.getAngleRotation()
+		int direction = directionator.goForwardsOrBackwards();
+		int desired_rot = directionator.getAngleRotation();
+		int x_dist = abs(simulator.getX() - newX);
+		int y_dist = abs(simulator.getY() - newY);
+
 		if(Math.abs(rotated) < Math.abs(desired_rot)){
-			int rot_direction = -desired_rot/Math.abs(desired_rot)
+			int rot_direction = -desired_rot/Math.abs(desired_rot);
 			rotated += 10;
 			int distance_to_move = Math.PI*bot_diameter*(10/360);
 			return rot_direction * distance_to_move*360 / wheel_circum;
 		}
-
-		int x_dist = abs(simulator.getX() - newX)
-		int y_dist = abs(simulator.getY() - newY)
-
-		else if(x_dist < 3 and y_dist < 3){
-			return 36*direction
+		else if(x_dist < 3 && y_dist < 3){
+			return 36*direction;
 		}
 		else{
-			rotated = 0
-			return 0
+			rotated = 0;
+			return 0;
 		}
 	}
 
