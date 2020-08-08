a096d381479220816f252e41ac03dac6cb901b2c
Austin Shih <austin2shih@yahoo.com>
2020-08-07 20:51:42 -0700

    Updated Motor Control now using the bot's current position to decide which motors to turn (still does not have access to the destination coords)

--- motorControl.java
+++ motorControl.java
@@ -6,31 +6,47 @@ public class motorControl{
 	int rotated = 0;
 
 	public static int leftMotorDegrees(int desired_rot, int direction){
-		// while we aren't at the desired rotation, rotate a little
-		// then we go in the direction bit by bit until we reach destination
-		// also must account for size of wheel but that should be ok
-		if(Math.abs(rotated) < Math.abs(desired_rot)){
-			int rot_direction = desired_rot/Math.abs(desired_rot)
-			rotated += 10;
-			int distance_to_move = Math.PI*bot_diameter*(10/360);
-			return rot_direction * distance_to_move*360 / wheel_circum;
-		}
-		else{
-			return 360*direction
-		}
-	}
-
-	public static int rightMotorDegrees(int desired_rot, int direction){
-		// same as above but for the other motor
+		int direction = directionator.goForwardsOrBackwards()
+		int desired_rot = directionator.getAngleRotation()
 		if(Math.abs(rotated) < Math.abs(desired_rot)){
 			int rot_direction = -desired_rot/Math.abs(desired_rot)
 			rotated += 10;
 			int distance_to_move = Math.PI*bot_diameter*(10/360);
 			return rot_direction * distance_to_move*360 / wheel_circum;
 		}
+
+		int x_dist = abs(simulator.getX() - newX)
+		int y_dist = abs(simulator.getY() - newY)
+
+		else if(x_dist < 3 and y_dist < 3){
+			return 36*direction
+		}
 		else{
-			return 360*direction
+			rotated = 0
+			return 0
 		}
 	}
-    
+
+	public static int rightMotorDegrees(){
+		int direction = directionator.goForwardsOrBackwards()
+		int desired_rot = directionator.getAngleRotation()
+		if(Math.abs(rotated) < Math.abs(desired_rot)){
+			int rot_direction = -desired_rot/Math.abs(desired_rot)
+			rotated += 10;
+			int distance_to_move = Math.PI*bot_diameter*(10/360);
+			return rot_direction * distance_to_move*360 / wheel_circum;
+		}
+
+		int x_dist = abs(simulator.getX() - newX)
+		int y_dist = abs(simulator.getY() - newY)
+
+		else if(x_dist < 3 and y_dist < 3){
+			return 36*direction
+		}
+		else{
+			rotated = 0
+			return 0
+		}
+	}
+
 }
