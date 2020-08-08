c917461a2ff7219f6a71d2eb6fcca3f6b4e1caa4
Deerop <xchenbox@gmail.com>
2020-08-07 22:02:09 -0700

    PseudoCode for Main Class

--- src/RoboSimp.java
+++ src/RoboSimp.java
@@ -4,5 +4,15 @@ public class RoboSimp{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Enter destination x:");
+        int xDest = scan.nextInt();
+
+        System.out.println("Enter destination y:");
+        int yDest = scan.nextInt();
+
+        directionator.setDestination(xDest, yDest);
+        simulator.update();
+        //directionator.take x, y, and rot from simulator()
+        display.updateRobotGraphics();
     }
 }
