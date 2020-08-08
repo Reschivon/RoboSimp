1c7429c73bd0b172f6273ea6789a5ae8528af147
Deerop <xchenbox@gmail.com>
2020-08-07 21:49:50 -0700

    Update display

--- src/display.java
+++ src/display.java
@@ -1,35 +1,35 @@
-package javatutorial.net;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class graphics extends JPanel {
+public class display{
     // initialize class variables
     int x;
     int y;
     int rotation;
 
-    public void blitPose(Graphics g) {
-        g.drawString("Robot x = " + x.toString(), 10, 10);
-        g.drawString("Robot y = " + y.toString(), 10, 50);
-        g.drawString("Robot rotation = " + rotation.toString(), 10, 100);
-
-    }
-
-    public void displayRobotPositions() {
-        JFrame frame = new JFrame("Robot Simulator");
-        frame.getContentPane().add(new graphics());
-        frame.setSize(300, 300);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-    }
-
-    public void updateRobotGraphics(){
+    public void updateRobotGraphics() {
         x = simulator.getX();
         y = simulator.getY();
         rotation = simulator.getRotation();
+    }
 
-        blitPose();
+    public display() {
+        JFrame frame = new JFrame("Robot Simulator");
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(new JPanel() {
+
+            public void paintComponent(Graphics g) {
+                g.drawString("Robot x = " + x, 10, 10);
+                g.drawString("Robot y = " + y, 10, 50);
+                g.drawString("Robot rotation = " + rotation, 10, 100);
+
+                repaint();
+            }
+
+        });
     }
 }
