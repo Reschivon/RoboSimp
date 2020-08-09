fc0da1c15fef170f38a91f00b9f1f39821b1af91
Radagast-the-infinite <ng.kyle101@gmail.com>
2020-08-08 20:47:24 -0700

    Blitting the String

--- src/displayFooData.java
+++ src/displayFooData.java
@@ -8,6 +8,23 @@ public class displayFooData {
     int y;
     int rotation;
 
+    public displayFooData() {
+        JFrame frame = new JFrame("Robot Simulator");
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(new JPanel() {
+            public void paintComponent(Graphics g) {
+                g.drawString("Robot x = " + x, 10, 20);
+                g.drawString("Robot y = " + y, 10, 60);
+                g.drawString("Robot rotation = " + rotation, 10, 100);
+                repaint();
+            }
+
+        });
+    }
+
     public int getInput(String message){
         Scanner scan = new Scanner(System.in);
         System.out.println("Set " + message + ":");
@@ -22,24 +39,6 @@ public class displayFooData {
         rotation = getInput("Rotation");
     }
 
-    public displayFooData() {
-        JFrame frame = new JFrame("Robot Simulator");
-        frame.setSize(300, 300);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.add(new JPanel() {
-
-            public void paintComponent(Graphics g) {
-                g.drawString("Robot x = " + x, 10, 10);
-                g.drawString("Robot y = " + y, 10, 50);
-                g.drawString("Robot rotation = " + rotation, 10, 100);
-
-                repaint();
-            }
-
-        });
-    }
 
     // Initialize Getters
     public int getX(){
