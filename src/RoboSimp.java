d2b1cafaa3d4e4eb6ad671420630c4622524176c
Radagast-the-infinite <ng.kyle101@gmail.com>
2020-08-08 20:27:49 -0700

    Can input data now

--- src/RoboSimp.java
+++ src/RoboSimp.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 public class RoboSimp{
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String input = scan.next();
-        System.out.println(input);
+        displayFooData robotDisplay = new displayFooData();
+        robotDisplay.updateRobotPose();
     }
+
+
 }
--- src/displayFooData.java
+++ src/displayFooData.java
@@ -1,34 +1,53 @@
-//import javax.swing.*;
-//import java.awt.*;
-//
-//public class displayFooData {
-//    // initialize class variables
-//    int x;
-//    int y;
-//    int rotation;
-//
-//    public void updateRobotPose() {
-//        x = simulator.getX();
-//        y = simulator.getY();
-//        rotation = simulator.getRotation();
-//    }
-//
-//    public displayFooData() {
-//        JFrame frame = new JFrame("Robot Simulator");
-//        frame.setSize(300, 300);
-//        frame.setVisible(true);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        frame.add(new JPanel() {
-//
-//            public void paintComponent(Graphics g) {
-//                g.drawString("Robot x = " + x, 10, 10);
-//                g.drawString("Robot y = " + y, 10, 50);
-//                g.drawString("Robot rotation = " + rotation, 10, 100);
-//
-//                repaint();
-//            }
-//
-//        });
-//    }
-//}
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+public class displayFooData {
+    // initialize class variables
+    int x;
+    int y;
+    int rotation;
+
+    public int getInput(String message){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Set " + message + ":");
+        String input = scan.next();
+
+        return Integer.parseInt(input);
+    }
+
+    public void updateRobotPose() {
+        x = getInput("X");
+        y = getInput("Y");
+        rotation = getInput("Rotation");
+    }
+
+    public displayFooData() {
+        JFrame frame = new JFrame("Robot Simulator");
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(new JPanel() {
+
+            public void paintComponent(Graphics g) {
+                g.drawString("Robot x = " + x, 10, 10);
+                g.drawString("Robot y = " + y, 10, 50);
+                g.drawString("Robot rotation = " + rotation, 10, 100);
+
+                repaint();
+            }
+
+        });
+    }
+
+    // Initialize Getters
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){return y;}
+
+    public int getRotation(){return rotation;}
+
+}
