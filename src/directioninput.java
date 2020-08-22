2a2bf95664cb92d99a82169e175a604bef8297b6
UpsideDownTurtle <theajaybodla@gmail.com>
2020-08-21 18:48:13 -0700

    Created Input Code

---
+++ src/directioninput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class directioninput {
+        public static void main(String[] args) {
+            int xpoint;
+            int ypoint;
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Enter the x coordinate");
+            xpoint = myObj.nextInt();
+            System.out.println("Enter the y coordinate");
+            ypoint = myObj.nextInt();
+    }
+}
